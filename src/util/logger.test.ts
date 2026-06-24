f03720bc0da2ffbc23eb0b04dd0c7dcf87ac181d
milosbugarinovic <milos.bugarinovic@gmail.com>
2026-06-25 00:30:24 +0200

    refactor: remove babel build pipeline
    BREAKING CHANGE:
    - drop all @babel dev dependencies
    - remove babel.config.cjs and ts-node/tsc-alias
    - update node to v22.22.1 in .nvmrc
    - add nvm setup to husky hooks
    - update typescript, vitest, and commitlint
    - clean up lib references from ignore files

--- src/util/logger.test.ts
+++ src/util/logger.test.ts
@@ -1,4 +1,4 @@
-import { LoggerStrategyVoid } from '@beecode/msh-logger/logger-strategy/void'
+import { PresetVoid } from '@beecode/msh-logger/controller/preset/void'
 import { describe, expect, it } from 'vitest'
 
 import { logger, setAppBootLogger } from '#src/util/logger.js'
@@ -7,14 +7,14 @@ describe('logger', () => {
 	describe('appBootLogger', () => {
 		it('should retrieve default logger', () => {
 			const defaultLogger = logger()
-			expect(defaultLogger instanceof LoggerStrategyVoid).toBeTruthy()
+			expect(defaultLogger instanceof PresetVoid).toBeTruthy()
 		})
 
 		it('should allow to change logger', () => {
-			const newLogger = new LoggerStrategyVoid()
+			const newLogger = new PresetVoid()
 			setAppBootLogger(newLogger)
 			const currentLogger = logger()
-			expect(currentLogger instanceof LoggerStrategyVoid).toBeTruthy()
+			expect(currentLogger instanceof PresetVoid).toBeTruthy()
 		})
 	})
 })
